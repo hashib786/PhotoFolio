3b6ce8026ad953547f793982862240c8105d889b
Hashib Raja <hashibraja23@gmail.com>
2023-09-20 06:51:53 +0530

    Created Reusable buttons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import Navbar from "./Navbar";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Button
+        variant="fill"
+        size="medium"
+        text="Click me"
+        onClick={() => console.log("Button clicked")}
+      />
+      <Button
+        variant="outline"
+        size="small"
+        text="Outline"
+        bgColor="#ff58330"
+        textColor="#ffffff"
+      />
     </>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+function Button({ variant, size, text, textColor, bgColor, onClick }) {
+  const classes = `button ${variant} ${size}`;
+  const styles = {
+    color: textColor,
+    backgroundColor: bgColor,
+  };
+
+  return (
+    <button
+      className={classes}
+      style={variant === "outline" ? {} : styles}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  variant: PropTypes.oneOf(["fill", "outline"]),
+  size: PropTypes.oneOf(["small", "medium"]),
+  text: PropTypes.string.isRequired,
+  textColor: PropTypes.string,
+  bgColor: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  variant: "fill",
+  size: "medium",
+  textColor: "#ffffff", // Default text color (white)
+  bgColor: "#333", // Default background color (dark gray)
+};
+
+export default Button;
