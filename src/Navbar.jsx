eabf6a388ef3abdc0380108c9212e791dcb653cb
Hashib Raja <hashibraja23@gmail.com>
2023-09-20 06:34:14 +0530

    Created Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img
+          src="https://stalwart-wisp-382f3c.netlify.app/assets/logo.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="project-name">PhotoFolio</div>
+    </nav>
+  );
+}
+
+export default Navbar;
